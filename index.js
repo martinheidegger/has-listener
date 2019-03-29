8b88457206c40ffd66023fd5d5abe326d3cd2332
Martin Heidegger <martin.heidegger@gmail.com>
2019-03-29 20:29:53 +0900

    reduced complexity

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 'use strict'
 
-exports.hasListener = function hasListener (target, targetEvent, handler) {
-  let count = target.listenerCount(targetEvent)
-  target.on('newListener', newListener)
-  target.on('removeListener', removeListener)
-
+function createCounter (targetEvent, count, handler) {
   handler(count > 0)
+  return {
+    up (event) {
+      if (event !== targetEvent) return
+      count += 1
+      if (count === 1) {
+        handler(true)
+      }
+    },
+    down (event) {
+      if (event !== targetEvent) return
+      count -= 1
+      if (count === 0) {
+        handler(false)
+      }
+    }
+  }
+}
+
+exports.hasListener = function hasListener (target, targetEvent, handler) {
+  let counter = createCounter(targetEvent, target.listenerCount(targetEvent), handler)
+  target.listenerCount(targetEvent)
+  target.on('newListener', counter.up)
+  target.on('removeListener', counter.down)
 
   return function stopLookingForListener () {
-    target.removeListener('newListener', newListener)
-    target.removeListener('removeListener', removeListener)
-  }
-
-  function newListener (event) {
-    if (event !== targetEvent) {
-      return
-    }
-    count += 1
-    if (count === 1) {
-      handler(true)
-    }
-  }
-
-  function removeListener (event) {
-    if (event !== targetEvent) {
-      return
-    }
-    count -= 1
-    if (count === 0) {
-      handler(false)
-    }
+    target.removeListener('newListener', counter.up)
+    target.removeListener('removeListener', counter.down)
   }
 }
