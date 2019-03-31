08f17de3abd48fff6de49a61d2ecaf3c9d7a3c86
Martin Heidegger <martin.heidegger@gmail.com>
2019-04-01 02:27:40 +0900

    typescript: fixed definitions file.

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,4 @@ import { EventEmitter } from 'events'
 
 type StopListening = () => void
 
-declare function hasListener (target: EventEmitter, event: string | Array<string> | Set<string>, handler: (hasHandler: boolean) => void): StopListening
+declare function hasListener (target: EventEmitter, event: string | string[] | Set<string>, handler: (hasHandler: boolean) => void): StopListening
